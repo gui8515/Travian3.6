25e211f25eb0ee20ebccf25d0ce146a9c83aeb8c
Guilherme Gois Pereira <gui8515@gmail.com>
2022-06-13 11:52:04 -0300

    init

---
+++ anmelden.php
@@ -0,0 +1,184 @@
+<?php
+
+#################################################################################
+##              -= YOU MAY NOT REMOVE OR CHANGE THIS NOTICE =-                 ##
+## --------------------------------------------------------------------------- ##
+##  Filename       anmelden.php                                                 ##
+##  Developed by:  Dzoki                                                       ##
+##  License:       TravianX Project                                            ##
+##  Copyright:     TravianX (c) 2010-2011. All rights reserved.                ##
+##                                                                             ##
+#################################################################################
+
+use App\Utils\AccessLogger;
+
+if(!file_exists('var/installed') && @opendir('install')) {
+    header("Location: install/");
+    exit;
+}
+
+include('GameEngine/Account.php');
+AccessLogger::logRequest();
+
+$invited=(isset($_GET['uid'])) ? filter_var($_GET['uid'], FILTER_SANITIZE_NUMBER_INT):$form->getError('invt');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+	<head>
+	<title><?php echo SERVER_NAME; ?> - Registration</title>
+		<link rel="shortcut icon" href="favicon.ico"/>
+	<meta name="content-language" content="en" />
+	<meta http-equiv="cache-control" content="max-age=0" />
+	<meta http-equiv="imagetoolbar" content="no" />
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	<script src="mt-core.js?0faab" type="text/javascript"></script>
+	<script src="mt-more.js?0faab" type="text/javascript"></script>
+	<script src="unx.js?f4b7h" type="text/javascript"></script>
+	<script src="new.js?0faab" type="text/javascript"></script>
+	<link href="<?php echo GP_LOCATE; ?>lang/en/compact.css?f4b7i" rel="stylesheet" type="text/css" />
+	<link href="<?php echo GP_LOCATE; ?>lang/en/lang.css?f4b7d" rel="stylesheet" type="text/css" />
+	<link href="<?php echo GP_LOCATE ?>travian.css?f4b7d" rel="stylesheet" type="text/css" />
+		<link href="<?php echo GP_LOCATE ?>lang/en/lang.css" rel="stylesheet" type="text/css" />
+	   </head>
+
+<body class="v35 ie ie7" onload="initCounter()">
+
+<div class="wrapper">
+<div id="dynamic_header">
+</div>
+<div id="header"></div>
+<div id="mid">
+<?php include("Templates/menu.tpl");
+if(REG_OPEN == true){ ?>
+<div id="content"  class="signup">
+
+<h1><img src="img/x.gif" class="anmelden" alt="register for the game" /></h1>
+<h5><img src="img/x.gif" class="img_u05" alt="registration"/></h5>
+
+<p><?php echo BEFORE_REGISTER; ?></p>
+
+<form name="snd" method="post" action="anmelden.php">
+<input type="hidden" name="invited" value="<?php echo $invited; ?>" />
+<input type="hidden" name="ft" value="a1" />
+
+<table cellpadding="1" cellspacing="1" id="sign_input">
+	<tbody>
+		<tr class="top">
+			<th><?php echo NICKNAME; ?></th>
+			<td><input class="text" type="text" name="name" value="<?php echo $form->getValue('name'); ?>" maxlength="30" />
+			<span class="error"><?php echo $form->getError('name'); ?></span>
+			</td>
+		</tr>
+		<tr>
+			<th><?php echo EMAIL; ?></th>
+			<td>
+				<input class="text" type="text" name="email" value="<?php echo stripslashes($form->getValue('email')); ?>" />
+				<span class="error"><?php echo $form->getError('email'); ?></span>
+				</td>
+			</tr>
+		<tr>
+			<th><?php echo PASSWORD; ?></th>
+			<td>
+				<input class="text" type="password" name="pw" value="<?php echo stripslashes($form->getValue('pw')); ?>" maxlength="100" />
+				<span class="error"><?php echo $form->getError('pw'); ?></span>
+			</td>
+		</tr>
+	</tbody>
+</table>
+
+<table cellpadding="1" cellspacing="1" id="sign_select">
+	<tbody>
+		<tr class="top">
+			<th><img src="img/x.gif" class="img_u06" alt="choose tribe" />
+			<input style="display: none;" class="radio" type="radio" name="kid" value="0" checked="checked" />
+		</th>
+		</tr>
+		<tr>
+			<td>
+				<label>
+				<b><input class="radio" type="radio" name="vid" value="1" <?php echo $form->getRadio('vid',1); ?> />&nbsp;<?php echo ROMANS; ?></b>
+					<ul class="characteristics">
+						<li>Pode aumentar ou estender um edifício e um campo de recursos simultaneamente</li>
+						<li>A Muralha da Cidade dá um bônus de defesa alto</li>
+						<li>Os comerciantes podem transportar 500 unidades de recursos (16 campos/hora)</li>
+						<li>Infantaria muito poderosa, cavalaria média</li>
+						<li>O desenvolvimento é caro e leva muito tempo</li>
+					</ul>
+				</label>
+			</td>
+		</tr><tr>
+			<td>
+				<label>
+				<b><input class="radio" type="radio" name="vid" value="2" <?php echo $form->getRadio('vid',2); ?> />&nbsp;<?php echo TEUTONS; ?></b>
+					<ul class="characteristics">
+						<li>Bônus de pilhagem: as fendas inimigas escondem apenas 80% da quantidade <br /> normal na versão 3.5 do Travian.</li>
+						<li>A parede de terra é quase indestrutível, mas só dá um bônus mínimo de defesa</li>
+						<li>Os comerciantes podem transportar 1000 unidades de recursos (12 campos/hora)</li>
+						<li>Tropas baratas e rápidas que são excelentes saqueadores</li>
+						<li>Defesa fraca</li>
+					</ul>
+				</label>
+			</td>
+		</tr><tr>
+			<td>
+				<label>
+					<b><input class="radio" type="radio" name="vid" value="3" <?php echo $form->getRadio('vid',3); ?> />&nbsp;<?php echo GAULS; ?></b>
+					<ul class="characteristics">
+						<li>Bônus de velocidade: tropas mais rápidas do jogo</li>
+						<li>A paliçada dá um bônus de defesa médio</li>
+						<li>Os comerciantes podem transportar 750 unidades de recursos (24 campos/hora)</li>
+						<li>Cranny é duas vezes maior</li>
+						<li>Armas de cerco caras</li>
+						<li>Colonos baratos</li>
+					</ul>
+				</label>
+			</td>
+		</tr>
+		
+	</tbody>
+</table>
+
+<ul class="important">
+<?php
+echo $form->getError('tribe');
+echo $form->getError('agree');
+?>
+		</ul>
+
+<p>
+		<input class="check" type="checkbox" name="agb" value="1" <?php echo $form->getRadio('agb',1); ?>/><?php echo ACCEPT_RULES; ?></p>
+
+<p class="btn">
+	<button value="anmelden" name="s1" id="btn_signup" class="trav_buttons" alt="register button"/> Register </button> 
+</p>
+</form>
+
+<p class="info"><?php echo ONE_PER_SERVER; ?></p>
+</div>
+<?php }else{ ?>
+<div id="content"  class="signup">
+
+<h1><img src="img/x.gif" class="anmelden" alt="register for the game" /></h1>
+<h5><img src="img/x.gif" class="img_u05" alt="registration"/></h5>
+
+<p><?php echo REGISTER_CLOSED; ?></p>
+</div>
+<?php } ?>
+<div id="side_info" class="outgame">
+<?php
+if(NEWSBOX1) { include("Templates/News/newsbox1.tpl"); }
+if(NEWSBOX2) { include("Templates/News/newsbox2.tpl"); }
+if(NEWSBOX3) { include("Templates/News/newsbox3.tpl"); }
+?>
+			</div>
+
+<div class="clear"></div>
+			</div>
+
+			<div class="footer-stopper outgame"></div>
+			<div class="clear"></div>
+
+<?php include("Templates/footer.tpl"); ?>
+<div id="ce"></div>
+</body>
+</html>
